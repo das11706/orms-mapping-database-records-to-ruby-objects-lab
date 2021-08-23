84582b70a6f8bfc3fa4e2d3091adffa941046673
Daniel Suarez <suarez.daniel101@gmail.com>
2021-08-23 16:55:51 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -37,6 +37,18 @@ class Student
       self.new_from_db(row)
     end.first
   end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
   
   def save
     sql = <<-SQL
